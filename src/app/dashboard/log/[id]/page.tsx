01c6b360fdfe3b851d209215cab3789e646e685b
vmthong18 <minhthongvo88@gmail.com>
2025-09-25 14:50:46 +0700

    vmthong

--- src/app/dashboard/log/[id]/page.tsx
+++ src/app/dashboard/log/[id]/page.tsx
@@ -106,6 +106,7 @@ export default async function logDetail({
                                                 day: '2-digit',
                                                 hour: '2-digit',
                                                 minute: '2-digit',
+                                                second:'2-digit',
                                                 hour12: false,
                                             })
                                             .replace(',', '')}
