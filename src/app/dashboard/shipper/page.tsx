dade1266f5da6b7165b04999944da5e56f76c4ea
vmthong18 <minhthongvo88@gmail.com>
2025-10-24 10:56:08 +0700

    zxc

--- src/app/dashboard/shipper/page.tsx
+++ src/app/dashboard/shipper/page.tsx
@@ -251,7 +251,7 @@ export default async function PageDiaDiem() {
     };
     return (
 
-         <main className="p-6">
+        <main className="p-6">
             <div className="flex items-center justify-between mb-6">
                 <h1 className="text-2xl font-bold">Vận chuyển</h1>
                 <span className="text-blue-500">100 đơn</span>
