808fb36ecb8a3847fe3133f6e7269cf08662726c
vmthong18 <minhthongvo88@gmail.com>
2026-05-12 11:42:10 +0700

    vmthong

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,6 +4,8 @@ import { useRouter, useSearchParams } from "next/navigation";
 import Image from "next/image";
 
 export default function LoginPage() {
+
+  
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
